31930344d64a30ed3218fb05ef1c16effa39a56e
AdiModak <50497897+AdiModak@users.noreply.github.com>
2019-09-07 14:18:10 +0530

    Update add.component.ts

--- add.component.ts
+++ add.component.ts
@@ -21,6 +21,10 @@ export class AddCompoenent {
         this._userService.addInfo(this.user);
         this.user = null;
         this.user = new User();
+         for (let index = 0; index < this.names.length; index++) {
+            localStorage.setItem('names', JSON.stringify(this.names));
+            console.log(localStorage.getItem('names'));
+        }
        //this.user.id = this.user.id ++;
         //console.log(this.user.id);
     }
